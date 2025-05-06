044bd620cd3c375a615164839a6eacff9dae66d4
S-N00B-1 <49575478+S-N00B-1@users.noreply.github.com>
2025-05-06 10:21:25 -0700

    Fix swimming when no hunger

--- src/main/java/io/github/sn00b1/oneblockswimming/mixins/ServerGamePacketListenerImplMixin.java
+++ src/main/java/io/github/sn00b1/oneblockswimming/mixins/ServerGamePacketListenerImplMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.entity.Pose;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,7 +19,7 @@ public class ServerGamePacketListenerImplMixin {
 
     @Inject(method = "handlePlayerInput", at= @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/server/level/ServerLevel;)V", shift = At.Shift.AFTER))
     public void setSwimming(ServerboundPlayerInputPacket packet, CallbackInfo ci) {
-        if (this.player.isInWater() && (packet.input().sprint() || this.player.isSprinting())) {
+        if (this.player.isInWater() && (packet.input().sprint() || this.player.isSprinting()) && this.canSprint()) {
             this.player.setSwimming(true);
             this.player.setPose(Pose.SWIMMING);
             this.player.setSprinting(true);
@@ -27,9 +28,14 @@ public class ServerGamePacketListenerImplMixin {
 
     @Inject(method = "handleMovePlayer", at= @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/server/level/ServerLevel;)V", shift = At.Shift.AFTER))
     public void setSwimming(ServerboundMovePlayerPacket packet, CallbackInfo ci) {
-        if (this.player.isInWater() && this.player.isSprinting()) {
+        if (this.player.isInWater() && this.player.isSprinting() && this.canSprint()) {
             this.player.setSwimming(true);
             this.player.setPose(Pose.SWIMMING);
         }
+    }
+
+    @Unique
+    private boolean canSprint() {
+        return (this.player.getFoodData().getFoodLevel() > 6.0F || this.player.getAbilities().mayfly);
     }
 }
